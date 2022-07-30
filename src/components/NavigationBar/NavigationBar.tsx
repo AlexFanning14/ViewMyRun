56ee595bfd4bb03f6868b4902c84f124191feaff
Alex Fanning <alexfanning14@gmail.com>
2022-07-30 17:29:59 +0800

    Initial Config and Theming

---
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from '../../pages/HomePage/HomePage';
+import LoginRedirect from '../../pages/LoginRedirect/LoginRedirect';
+
+const NavigationBar = () => {
+    return ( 
+            <div>
+                <BrowserRouter>
+                    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+                        <Container>
+
+                        <Navbar.Brand href="#home">View My Run</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav>
+                                <Nav.Link href="/">Home</Nav.Link>
+                                <Nav.Link href="/redirect">Contact Us</Nav.Link>
+                                <Nav.Link href="/contact-us">About Us</Nav.Link>
+                                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav>
+                        </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+                <Routes>
+                    <Route path="/" element={<HomePage/>} />
+                    <Route path="/redirect" element={<LoginRedirect/>} />
+                </Routes>
+                </BrowserRouter>
+            </div>
+    );
+};
+
+export default NavigationBar;
---
+++ src/pages/LoginRedirect/LoginRedirect.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginRedirect = () => {
+ 
+    return ( 
+    <div>
+        My Home Page
+    </div>
+    )
+};
+
+export default LoginRedirect;
